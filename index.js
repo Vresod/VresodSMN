bdd07961a005ba946c7fa8221c386385e4a8e67c
Vresod <lukebinkofsky@gmail.com>
2019-12-05 15:01:48 +0000

    basic variable system in place for html

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
-
+const fs = require("fs");
 const app = express();
+var html;
+var htmlvars = {
+	"testvar":"yeah"
+}
 
 app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+  res.send(html)
+});
+
+fs.readFile('index.html', 'utf8', function(err, contents) {
+    html = contents.replace("%testvar%",htmlvars.testvar);
 });
 
 app.listen(3000, () => {
   console.log('server started');
+	console.log(html)
 });
